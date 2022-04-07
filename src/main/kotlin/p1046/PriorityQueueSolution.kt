5b70d1b58bc5f5420189d26c5f3cde41b6ac59a0
nothing <mr.n01hing@gmail.com>
2022-04-07 18:58:28 +0400

    problem 1046 - problem solution

---
+++ src/main/kotlin/p1046/PriorityQueueSolution.kt
@@ -0,0 +1,32 @@
+package p1046
+
+import java.util.*
+
+class PriorityQueueSolution {
+    // Priority queue solution
+    fun lastStoneWeight(stones: IntArray): Int {
+        val heap = PriorityQueue<Int>(stones.size) { a, b -> b - a }
+
+        for (stone in stones) {
+            heap.add(stone)
+        }
+
+        while (heap.size > 1) {
+            val max1 = heap.remove()
+            val max2 = heap.remove()
+
+            val smash = max1 - max2
+            if (smash == 0) {
+                continue
+            }
+
+            heap.add(smash)
+        }
+
+        return if (heap.size == 1) {
+            heap.peek()
+        } else {
+            0
+        }
+    }
+}
