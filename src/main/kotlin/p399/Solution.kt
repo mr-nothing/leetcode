9ad7ea55758336429401a5b8043662238ccee9bb
nothing <mr.n01hing@gmail.com>
2022-05-01 20:27:27 +0400

    problem 399 - initial commit, problem description

---
+++ src/main/kotlin/p399/Solution.kt
@@ -0,0 +1,7 @@
+package p399
+
+class Solution {
+    fun calcEquation(equations: List<List<String>>, values: DoubleArray, queries: List<List<String>>): DoubleArray {
+        return doubleArrayOf()
+    }
+}
