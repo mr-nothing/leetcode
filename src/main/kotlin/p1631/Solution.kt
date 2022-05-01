b7ddf2d64354201e937f708a8936a4343a8f9d08
nothing <mr.n01hing@gmail.com>
2022-05-01 20:31:10 +0400

    problem 1631 - initial commit, problem description

---
+++ src/main/kotlin/p1631/Solution.kt
@@ -0,0 +1,9 @@
+package p1631
+
+class Solution {
+    fun minimumEffortPath(heights: Array<IntArray>): Int {
+
+
+        return 0
+    }
+}
---
+++ src/test/kotlin/p1631/SolutionTest.kt
@@ -0,0 +1,16 @@
+package p1631
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
+
+internal class SolutionTest {
+    @TestFactory
+    fun `test solution`() = listOf(
+        "" to 12
+    ).map { (input, expected) ->
+        dynamicTest("") {
+            assertEquals(true, true)
+        }
+    }
+}
