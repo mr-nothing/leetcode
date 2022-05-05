769a6a02f19f87adc535bc08e19f0100a8cdba30
nothing <mr.n01hing@gmail.com>
2022-05-05 18:53:05 +0400

    problem 950 - A bit more explanations

--- src/main/kotlin/solved/p950/Solution.kt
+++ src/main/kotlin/solved/p950/Solution.kt
@@ -3,15 +3,16 @@ package solved.p950
 import java.util.*
 
 class Solution {
+    // The idea is just to reverse the process described in the problem.
     fun deckRevealedIncreasing(deck: IntArray): IntArray {
-        val sorted = deck.sorted()
+        val sorted = deck.sorted() // sort the array, the final state
 
         val list = LinkedList<Int>()
 
-        list.addFirst(sorted[sorted.lastIndex])
+        list.addFirst(sorted[sorted.lastIndex]) // unreveal the largest card
         for (i in sorted.lastIndex - 1 downTo 0) {
-            list.addFirst(list.pollLast()) // move from bottom to the top
-            list.addFirst(sorted[i]) // unreveal
+            list.addFirst(list.pollLast()) // move card from the bottom to the top
+            list.addFirst(sorted[i]) // unreveal next largest card
         }
 
         return list.toIntArray()
